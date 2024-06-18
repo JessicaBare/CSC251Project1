b4586e10db1f2bbe2ac8b2d31b2184cd563a608b
Jessica Bare <jessicabare42@gmail.com>
2024-06-17 22:03:55 -0400

    File that contains policy information.

--- Policy.java
+++ Policy.java
@@ -8,7 +8,9 @@ public class Policy {
     public double height;
     public double weight;
 
-
+    /**
+     * Default Cconstructor
+     */
     public Policy() {
         this.policyNumber = 0;
         this.providerName = "";
@@ -20,6 +22,17 @@ public class Policy {
         this.weight = 0;
     }
 
+    /**
+     * Paramterized Constructor
+     * @param policyNumber - the policy number
+     * @param providerName - the name of the provider
+     * @param firstName - policy holder's first name
+     * @param lastName - policy holder's last name
+     * @param age - policy holder's age
+     * @param smoking - does the policy holder smoke
+     * @param height - policy holder's height
+     * @param weight - policy holder's weight
+     */
     public Policy(int policyNumber, String providerName, String firstName, 
         String lastName, int age, String smoking, double height, double weight) {
         this.policyNumber = policyNumber;
@@ -32,11 +45,19 @@ public class Policy {
         this.weight = weight;
     }
 
+    /**
+     * Calculates the BMI
+     * @return bmi - the calculated bmi
+     */
     public double calculateBMI() {
         double bmi = (this.weight * 703 / (this.height * this.height));
         return bmi;
     }
 
+    /**
+     * Calculates the policy price
+     * @return calculated price
+     */
     public double calculatePolicyPrice() {
         double basePrice = 600;
         if (this.age > 50) {
@@ -50,5 +71,19 @@ public class Policy {
             basePrice += (bmi - 35) * 20;
         }
         return basePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Policy Number: " + policyNumber + "\n" +
+                "Provider Name: " + providerName + "\n" +
+                "Policyholder’s First Name: " + firstName + "\n" +
+                "Policyholder’s Last Name: " + lastName + "\n" +
+                "Policyholder’s Age: " + age + "\n" +
+                "Policyholder’s Smoking: " + smoking + "\n" +
+                "Policyholder’s Height: " + height + "\n" +
+                "Policyholder’s Weight: " + weight + "\n" +
+                "Policyholder’s BMI: " + String.format("%.2f", calculateBMI()) + "\n" +
+                "Policy Price: " + String.format("$%.2f", calculateBMI()) + "\n";
     }
 }
